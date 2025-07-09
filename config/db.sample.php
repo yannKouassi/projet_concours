67ebe2906c7219e49ba542ebd6e23956b7de33a3
yannick_kouassi <ynnkouassi@gmail.com>
2025-07-09 15:11:33 +0000

    modification : ajout de FPDF qui est une classe PHP qui permet de générer des fichiers PDF en pur PHP.

---
+++ config/db.sample.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$host = 'localhost';
+$dbname = 'nom_de_votre_base';
+$username = 'votre_utilisateur';
+$password = 'votre_mot_de_passe';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
+;
